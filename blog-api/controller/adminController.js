87af7d0a7de49a30f8b43ded2ac6c30be3699e7a
Jitesh Prajapat <60288640+jiteshp13@users.noreply.github.com>
2021-06-20 15:08:02 +0530

    Add admin/login route

--- blog-api/controller/adminController.js
+++ blog-api/controller/adminController.js
@@ -13,3 +13,20 @@ exports.signup = (req, res) => {
         res.status(500).send("Error creating admin");
     })
 };
+
+exports.login = (req, res) => {
+    let { email, password } = req.body;
+    Admin.findOne({ email: email, password: password})
+    .then((userExists) => {
+        if(userExists) {
+            console.log(`Welcome back, ${email}`);
+            return res.status(200).send("Admin successfully logged in");
+        }
+        console.log("Wrong username or password");
+        return res.status(404).send("Admin not found");
+    })
+    .catch((error) => {
+        console.error(error);
+        return res.status(500).send(error);
+    });
+};
--- blog-api/routes/adminRoute.js
+++ blog-api/routes/adminRoute.js
@@ -4,5 +4,6 @@ const adminController = require("../controller/adminController");
 const router = express();
 
 router.post("/signup", adminController.signup);
+router.get("/login", adminController.login);
 
 module.exports = router;
