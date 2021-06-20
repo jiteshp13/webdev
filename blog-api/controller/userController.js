a14404669e152e81207e0133a87ea3d653febf7d
Jitesh Prajapat <60288640+jiteshp13@users.noreply.github.com>
2021-06-20 20:32:11 +0530

    Add user/postBlog route
    - Updated dependency: axios

--- blog-api/controller/userController.js
+++ blog-api/controller/userController.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const axios = require("axios");
 const User = require("../models/User");
+const Blog = require("../models/Blog");
 
 exports.signup = (req, res) => {
     let { firstName, lastName, email, password, dob } = req.body;
@@ -23,11 +26,11 @@ exports.login = (req, res) => {
             return res.status(200).send("User successfully logged in");
         }
         console.log("Wrong username or password");
-        return res.status(404).send("User not found");
+        return res.status(404).send("Wrong username or password");
     })
     .catch((error) => {
         console.error(error);
-        return res.send(500).send(error);
+        return res.send(500).send("Error logging in");
     });
 };
 
@@ -38,13 +41,39 @@ exports.getUser = (req, res) => {
     User.findOne({ _id: id })
     .then((userExists) => {
         if(userExists) {
-            console.log("User exists");
-            return res.status(200).send("User exists");
+            console.log(`User found: ${userExists.firstName} ${userExists.lastName}`);
+            return res.status(200).send(`User found: ${userExists.firstName} ${userExists.lastName}`);
         }
         console.error("User does not exists");
         return res.status(404).send("User does not exists");
     }).catch((error) => {
         console.error(error);
         return res.status(500).send(error);
+    });
+};
+
+let PORT = 7000;
+
+exports.postBlog = (req, res) => {
+    let { heading, blog, userId } = req.body;
+    let newBlog = new Blog({ heading, blog, userId });
+
+    // check if user has an account
+    axios.get(`http://localhost:${PORT}/user/getUser/${userId}`)
+    .then((response) => {
+        // POST a blog is user has an account
+        if(response.status === 200) {
+            let author = response.data.slice(12);
+            newBlog.save().then(() => {
+                console.log(`Hurray! Your blog has been posted ${author}`);
+                return res.status(200).send(`Hurray! Your blog has been posted ${author}`);
+            }).catch((error) => {
+                console.log("Error while posting the blog", error);
+                return res.status(500).send("Error while posting the blog");
+            });
+        }
+    }).catch((error) => {
+        console.error(`User with ${userId} does not exist`);
+        return res.status(401).send("Sign up to post blogs");
     });
 };
--- blog-api/routes/userRoute.js
+++ blog-api/routes/userRoute.js
@@ -6,5 +6,6 @@ const router = express();
 router.post("/signup", userController.signup);
 router.get("/login", userController.login);
 router.get("/getUser/:id", userController.getUser);
+router.post("/postBlog", userController.postBlog);
 
 module.exports = router;
